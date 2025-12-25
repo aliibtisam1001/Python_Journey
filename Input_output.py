aa920410a142f7034cebe623cbd6d88cf3e626f4
aliibtisam1001 <204402031+aliibtisam1001@users.noreply.github.com>
2025-12-26 02:27:11 +0800

    Add files via upload

---
+++ Input_output.py
@@ -0,0 +1,13 @@
+# DAY 1 OF LEARNING PYTHON FOR DATA SCIENCE 
+
+Name = input("Enter your name:")
+print(f"My name is {Name}\n")
+
+x,y,z=map(int , input("Enter three numbers you hate:").split())
+print(f"{x} {y} {z}\n")
+name="Ali Ibtisam"
+age=20
+c='A'
+boolean="True"
+print(f"I am {name}.\nI am {age} years old.\n{c} is {boolean}.\n")
+
---
+++ Variables.py
@@ -0,0 +1,19 @@
+a = "Hello ali"
+b = 10
+c = 11.22
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+x, y, z = 1, 2.5, "Python"
+print(x, y, z) 
+
+x = 10
+print(x) 
+del x
+
+# getting all keywords of python 
+import keyword
+print("The list of keywords are : ")
+print(keyword.kwlist)
